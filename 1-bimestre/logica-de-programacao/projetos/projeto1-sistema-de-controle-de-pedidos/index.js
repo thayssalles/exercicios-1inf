a744fc1c0d4c5a742b9aff95247efb313d0b914a
Thays Salles <thays.silva44@aluno.ce.gov.br>
2026-09-08 17:55:24 +0000

    projeto pronto

--- 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
+++ 1-bimestre/logica-de-programacao/projetos/projeto1-sistema-de-controle-de-pedidos/index.js
@@ -1,10 +1,30 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
-
-
-
-
-
-
+console.log("SISTEMAS DE CONTROLES E PEDIDOS");
+const cliente = "Mariana Silva"
+const cidade = "Fortaleza"
+const produto = "Notbook Pro"
+const categoria = "Notebook"
+const preco = 3500
+const quantidade = 2
+const  descontoPercentual = 10
+const valorPago = 7000
+const subtotal = (preco * quantidade)
+const valorDesconto = (subtotal * descontoPercentual/100)
+const valorFinal = subtotal - valorDesconto
+const troco = valorPago - valorFinal
+const resumo = `cliente:${cliente}
+cidade:${cidade}
+produto:${produto}
+categoria:${categoria}
+preco:${preco}
+quantidade:${quantidade}
+descontoPercentual:${descontoPercentual}
+valorPago:${valorPago}
+subtotal:${subtotal}
+valordesconto:${valorDesconto}
+valorFinal:${valorFinal}
+troco:${troco}`
+console.log(resumo)
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
 module.exports = {
